8d15ae41f6491162e8742bf719a13180dd3c9ef5
alex zimmerman <alex.zimmerman.info@gmail.com>
2021-11-11 10:23:43 +0300

    add dummy data

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function EventPage() {
+  return (
+    <div>
+      <h1>filtered event page</h1>
+    </div>
+  );
+}
+
+export default EventPage;
---
+++ pages/events/[id].js
@@ -0,0 +1,9 @@
+function EventPage() {
+  return (
+    <div>
+      <h1>Some event page</h1>
+    </div>
+  );
+}
+
+export default EventPage;
